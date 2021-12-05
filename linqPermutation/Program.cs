f98b8822b45bad2b3e17c25fdcc05ab3cf1979cd
Ning <xiaoningning@yahoo.com>
2021-12-04 23:00:22 -0800

    linq permutation 4.72 net framework

--- linqPermutation/Program.cs
+++ linqPermutation/Program.cs
@@ -12,11 +12,16 @@
         var r2 = iep.Permutate(str);
         foreach (var r in r2) Console.WriteLine(string.Join(",", r));
 
-        var r3 = iep.PermutateStringList(new List<string>() { "a", "b", "c" });
+        var r3 = iep.PermutateStringArray(new string[] { "a", "b", "c" });
         foreach (var r in r3) Console.WriteLine(string.Join(",", r));
 
-        var r4 = iep.PermutateStringList(new int[] { 1, 2, 3 });
+        var r4 = iep.PermutateIntList(new int[] { 1, 2, 3 });
         foreach (var r in r4) Console.WriteLine(string.Join(",", r));
+
+        Console.WriteLine("uenumerable permutation old framework");
+        var permOld = new IEnumerablePermutationNetFramework();
+        var ro4 = permOld.PermutateIntList(new int[] { 1, 2, 3 });
+        foreach (var r in ro4) Console.WriteLine(string.Join(",", r));
     }
 }
 public class IEnumerablePermutation
@@ -36,7 +41,7 @@ public class IEnumerablePermutation
             );
     }
 
-    public IEnumerable<string[]> PermutateStringList(List<string> source)
+    public IEnumerable<string[]> PermutateStringArray(string[] source)
     {
         return permutate(source, Enumerable.Empty<string>());
 
@@ -51,7 +56,7 @@ public class IEnumerablePermutation
             );
     }
 
-    public IEnumerable<int[]> PermutateStringList(int[] source)
+    public IEnumerable<int[]> PermutateIntList(int[] source)
     {
         return permutate(source, Enumerable.Empty<int>());
 
@@ -64,5 +69,33 @@ public class IEnumerablePermutation
                     prefix.Append(c)
                 )
             );
+    }
+}
+
+public class IEnumerablePermutationNetFramework
+{
+    public IEnumerable<T[]> Permutate<T>(IEnumerable<T> source)
+    {
+        return Permutation(source, Enumerable.Empty<T>());
+    }
+    IEnumerable<T[]> Permutation<T>(IEnumerable<T> reminder, IEnumerable<T> prefix)
+    {
+        return !reminder.Any() ?
+        new[] { prefix.ToArray() } :
+        reminder.SelectMany(
+            (c, i) => Permutation(
+                reminder.Take(i).Concat(reminder.Skip(i + 1)).ToArray(),
+                prefix.Append(c)
+            )
+        );
+    }
+    public IEnumerable<string[]> PermutateStringArray(string[] source)
+    {
+        return Permutation(source, Enumerable.Empty<string>());
+    }
+
+    public IEnumerable<int[]> PermutateIntList(int[] source)
+    {
+        return Permutation(source, Enumerable.Empty<int>());
     }
 }
